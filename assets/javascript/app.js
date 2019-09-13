3cf9ac98fc8472b1b9f1d4713a91af52d04b4dfd
JeffM <jeff@jeffreymclean.com>
2019-09-12 21:41:05 -0700

    Added Next Arrival and Minutes Away calculation. Added dynmaic insertion for the table from firebase data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,7 +34,9 @@ $("#submit-button").on("click", function(event){
     firstTime = $("#first-train-time").val().trim();
     trainFrequency = $("#train-frequency").val().trim();
 
-    console.log(trainName, trainDestination, firstTime, trainFrequency);
+ 
+
+    console.log(trainName, trainDestination, firstTime, trainFrequency); // --- Sanity Check
 
     database.ref("trains/").push({
         tName: trainName,
@@ -44,7 +46,45 @@ $("#submit-button").on("click", function(event){
     });
 })
 
+// -------------- Grabs all Childs from Database and puts into Table ------
+database.ref("trains/").on("child_added", function(snapshot){ 
+    console.log("Train: " + snapshot.val().tName);                // --
+    console.log("Destination: " + snapshot.val().tDestination);   // --  Sanity Checks
+    console.log("Start: " + snapshot.val().fTime);                // --
+    console.log("Frequency: " + snapshot.val().tFrequency);       // --
 
+var currentTime = moment();
+var firstTimeConv = moment(snapshot.val().fTime, "HH:mm").subtract(1,"years");
+trainFrequency = snapshot.val().tFrequency;
+
+console.log (currentTime);      // ---
+console.log(firstTimeConv);     // --- Sanity Checks
+console.log(trainFrequency);    // ---
+
+var diffInTime = moment().diff(moment(firstTimeConv), "minutes" );
+var timeRemain = diffInTime % trainFrequency;
+var nextTrainMin = trainFrequency - timeRemain;
+var nextTrainTime = moment().add(nextTrainMin, "minutes");
+
+console.log (diffInTime);      // ---
+console.log(timeRemain);     // --- Sanity Checks
+console.log(nextTrainMin);    // ---
+console.log(nextTrainTime);    // ---
+
+
+var newRow = $("<tr>")
+newRow.append("<td>" + snapshot.val().tName)
+    .append("<td>" + snapshot.val().tDestination)
+    .append("<td>" + snapshot.val().tFrequency)
+    .append("<td>" + moment(nextTrainTime).format("hh:mm"))
+    .append("<td>" + nextTrainMin);
+    
+
+$("#listTrains").append(newRow);
+
+
+
+})
 
 
 
