4691925516fb4d18ed22dba7faf831fd7fdf9c1a
JeffM <jeff@jeffreymclean.com>
2019-09-13 21:39:59 -0700

    Added background image, styling to CSS, and Created function clear trains with delete button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,6 +21,7 @@ $(document).ready(function () {
     var trainDestination;
     var firstTime;
     var trainFrequency;
+    var userID;
 
     var database = firebase.database(); 
 
@@ -36,7 +37,7 @@ $("#submit-button").on("click", function(event){
 
  
 
-    console.log(trainName, trainDestination, firstTime, trainFrequency); // --- Sanity Check
+    // console.log(trainName, trainDestination, firstTime, trainFrequency); // --- Sanity Check
 
     database.ref("trains/").push({
         tName: trainName,
@@ -48,47 +49,61 @@ $("#submit-button").on("click", function(event){
 
 // -------------- Grabs all Childs from Database and puts into Table ------
 database.ref("trains/").on("child_added", function(snapshot){ 
-    console.log("Train: " + snapshot.val().tName);                // --
-    console.log("Destination: " + snapshot.val().tDestination);   // --  Sanity Checks
-    console.log("Start: " + snapshot.val().fTime);                // --
-    console.log("Frequency: " + snapshot.val().tFrequency);       // --
+    // console.log("Train: " + snapshot.val().tName);                // --
+    // console.log("Destination: " + snapshot.val().tDestination);   // --  Sanity Checks
+   //  console.log("Start: " + snapshot.val().fTime);                // --
+   //  console.log("Frequency: " + snapshot.val().tFrequency);       // --
 
 var currentTime = moment();
 var firstTimeConv = moment(snapshot.val().fTime, "HH:mm").subtract(1,"years");
 trainFrequency = snapshot.val().tFrequency;
 
-console.log (currentTime);      // ---
-console.log(firstTimeConv);     // --- Sanity Checks
-console.log(trainFrequency);    // ---
+// console.log (currentTime);      // ---
+// console.log(firstTimeConv);     // --- Sanity Checks
+// console.log(trainFrequency);    // ---
 
 var diffInTime = moment().diff(moment(firstTimeConv), "minutes" );
 var timeRemain = diffInTime % trainFrequency;
 var nextTrainMin = trainFrequency - timeRemain;
 var nextTrainTime = moment().add(nextTrainMin, "minutes");
 
-console.log (diffInTime);      // ---
-console.log(timeRemain);     // --- Sanity Checks
-console.log(nextTrainMin);    // ---
-console.log(nextTrainTime);    // ---
+//  console.log (diffInTime);      ---
+// console.log(timeRemain);     // --- Sanity Checks
+// console.log(nextTrainMin);    // ---
+// console.log(nextTrainTime);    // ---
 
 
-var newRow = $("<tr>")
-newRow.append("<td>" + snapshot.val().tName)
-    .append("<td>" + snapshot.val().tDestination)
-    .append("<td>" + snapshot.val().tFrequency)
-    .append("<td>" + moment(nextTrainTime).format("hh:mm"))
-    .append("<td>" + nextTrainMin);
-    
+var newRow = $("<tr>").attr("id", snapshot.child().ref_.path.pieces_[1]).append(
+    $("<td>").text(snapshot.val().tName),
+    $("<td>").text(snapshot.val().tDestination),
+    $("<td>").text(snapshot.val().tFrequency),
+    $("<td>").text(moment(nextTrainTime).format("hh:mm")),
+    $("<td>").text(nextTrainMin),
+    $("<td>").attr("data-dbid", snapshot.child().ref_.path.pieces_[1]).attr("id", "remove").append('<i class="far fa-times-circle"></i>')
+)
 
 $("#listTrains").append(newRow);
+
+console.log(snapshot.child().ref_.path.pieces_[1])
 
 
 
 })
 
 
+// ------------------- Function to Remove Trains from Database on Click -----------------------
+$(document).on("click", "#remove", function(){
+    userID = $(this).attr("data-dbid");
 
+    database.ref("trains/").child(userID).remove();
 
+})
+
+// ------------------------ Clears Out Deleted Irem ----------------------------------------
+database.ref("trains/").on("child_removed", function(){ 
+    
+    $("#"+userID).empty();
+})
 
 
 });
